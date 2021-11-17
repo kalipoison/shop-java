af7c2d75d6b9d6b9bc173615b6711b68781100eb
kalipoison <1174616074@qq.com>
2021-11-17 15:12:34 +0800

    修复mybatis-plus 3.4.0版本之后，分页不生效问题

--- shop-Server/shop-common/src/main/java/com/gohb/config/MybatisPlusConfig.java
+++ shop-Server/shop-common/src/main/java/com/gohb/config/MybatisPlusConfig.java
@@ -1,5 +1,8 @@
 package com.gohb.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +12,26 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan(basePackages = {"com.gohb.mapper"})
 public class MybatisPlusConfig {
 
-    /**
-     * 做分页
-     * @return
-     */
+//    /**
+//     * 做分页
+//     * @return
+//     */
+//    @Bean
+//    public PaginationInnerInterceptor paginationInnerInterceptor(){
+//        return new PaginationInnerInterceptor(DbType.MYSQL);
+//    }
+
+    // mybatis-plus 3.4.0 之后采用以下方式进行分页
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor(){
-        return new PaginationInnerInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 
 }
